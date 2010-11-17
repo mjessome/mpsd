d0d7f5a7d7258484daa18e5ae368e5d226fee97e
Marc Jessome <marc.jessome@gmail.com>
2010-11-17 14:21:08 -0500

    Added license, readme, source info in daemon.py.

--- daemon.py
+++ daemon.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# Daemon header from:
+# http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
+
 import sys, os, time, atexit
 from signal import SIGTERM 
 

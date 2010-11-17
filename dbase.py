9dd90c050cdfa9103718f886a4be1e369ef7af32
Marc Jessome <marc.jessome@gmail.com>
2010-11-17 13:56:11 -0500

    move to git

---
+++ dbase.py
@@ -0,0 +1,196 @@
+import sqlite3
+import os
+import time
+
+def dbConnect(db_path):
+        """
+        Connect to the specified database
+        """
+        dne = False
+        c = None
+        db = None
+        
+        if not (os.access(db_path, os.F_OK)):
+                dne = True
+        
+        try:
+                db = sqlite3.connect(db_path)
+                c = db.cursor()
+        except err:
+                print "error connecting to database"
+                return None
+
+        c.execute('''PRAGMA foreign_key = ON''')
+        if dne:
+                c.execute('''CREATE TABLE artist (
+                                id              INTEGER, \
+                                name            TEXT, \
+                                PRIMARY KEY (id) \
+                                )''')
+                c.execute('''CREATE TABLE album ( \
+                                id              INTEGER, \
+                                title           TEXT, \
+                                date            INTEGER, 
+                                artist          INTEGER, \
+                                FOREIGN KEY (artist) REFERENCES artist (id), \
+                                PRIMARY KEY (id) \
+                                )''')
+                c.execute('''CREATE TABLE track ( \
+                                id              INTEGER, \
+                                num             INTEGER, \
+                                title           TEXT, \
+                                artist          INTEGER, \
+                                length          INTEGER, \
+                                genre           TEXT, \
+                                album           INTEGER, \
+                                FOREIGN KEY (album) REFERENCES album (id), \
+                                FOREIGN KEY (artist) REFERENCES artist (id) \
+                                PRIMARY KEY (id) \
+                                )''')
+                c.execute('''CREATE TABLE listened ( \
+                                track           INTEGER, \
+                                date            TEXT, \
+                                listentime      INTEGER, \
+                                FOREIGN KEY (track) REFERENCES track(id), \
+                                PRIMARY KEY (track, date) \
+                                )''')
+                                #Dates are stored "YYYY-MM-DD HH:MM:SS"
+                db.commit();
+
+        return db
+
+
+def getInfo(info):
+        """
+        Get all the info from the dictionary and return a full,
+        properly formatted dict that will be used
+        """
+        #Add a log of badly tagged files?
+
+        rval = {}
+
+        if info.has_key('albumartist'):
+                rval['albumartist'] = info['albumartist']
+        else:
+                rval['albumartist'] = ""
+
+        if info.has_key('artist'):
+                rval['artist'] = info['artist']
+                if(rval['albumartist'] == ""):
+                        rval['albumartist'] = rval['artist']
+        else:
+                if(rval['albumartist'] != ""):
+                        rval['artist'] = rval['albumartist']
+                else:
+                        rval['albumartist'] = "Unknown Artist"
+                        rval['artist'] = "Unknown Artist"
+
+        if info.has_key('date'):
+                rval['date'] = int(info['date'].rsplit("-")[0])
+        else:
+                rval['date'] = 0
+
+        if info.has_key('track'):
+                rval['track'] = int(info['track'].rsplit("/")[0])
+        else:
+                rval['track'] = 0
+
+        if info.has_key('title'):
+                rval['title'] = info['title']
+        else:
+                rval['title'] = "Unknown Track"
+
+        if info.has_key('album'):
+                rval['album'] = info['album']
+        else:
+                rval['album'] = "Unknown Album"
+
+        if info.has_key('genre'):
+                #check if genre is a list
+                if type(info['genre']).__name__ == 'list':
+                        rval['genre'] = info['genre'][0]
+                else:
+                        rval['genre'] = info['genre']
+        else:
+                rval['genre'] = "Unknown"
+
+        rval['time'] = int(info['time'])
+
+        return rval
+
+
+def dbUpdate(db, track):
+        """
+        Update the database with the given info
+        """
+
+        info = getInfo(track)
+
+        c = db.cursor()
+
+        id = {}
+
+        #Artist and AlbumArtist
+        for a in ("artist", "albumartist"):
+                c.execute('''SELECT id FROM artist \
+                                WHERE name=?''', [info[a]])
+                row = c.fetchone()
+                if row == None:
+                        #add the artist
+                        c.execute('''INSERT INTO artist VALUES \
+                                        (?,?)''', (None, info[a]))
+                        db.commit()
+                        id[a] = c.lastrowid
+                        print "\tNew Artist"
+                else:
+                        id[a] = int(row[0])
+
+                print "\t"+a+" id:" + str(a)
+
+                if(info['artist'] == info['albumartist']):
+                        #Must be a better way...
+                        id['albumartist'] = id[a]
+                        break
+
+        #Album
+        c.execute('''SELECT id FROM album WHERE title=?''', [info['album']])
+        row = c.fetchone()
+        if row == None:
+                #add the album
+                c.execute('''INSERT INTO album VALUES \
+                             (?, ?, ?, ?)''',
+                             (None, info['album'], info['date'],
+                                    id['albumartist']))
+                db.commit()
+                id['album'] = c.lastrowid
+
+                print "\tNew Album ID: " + str(id['album'])
+        else:
+                id['album'] = int(row[0])
+        
+        #Track
+        c.execute('''SELECT id,title \
+                   FROM track \
+                   WHERE title=? AND album=?''',
+                   (info['title'], id['album']))
+        row = c.fetchone()
+        if row == None:
+                #add the track
+                c.execute('''INSERT INTO track VALUES (?,?,?,?,?,?,?)''',
+                             (None, info['track'], info['title'], id['artist'],
+                              info['time'], info['genre'], id['album']))
+                db.commit()
+                id['track'] = c.lastrowid
+                print "\tNew Track"
+        else:
+                id['track'] = int(row[0])
+
+        #Listened Table insert
+        t = time.strftime('%Y-%m-%d %H:%M:%S')
+        c.execute('''INSERT INTO listened VALUES \
+                     (?, ?, 0)''',
+                     (id['track'], t))
+
+        db.commit()
+
+        return t
---
+++ mpdsd.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python
+
+import string
+import sys
+import time
+import mpd
+from socket import error as SocketError
+from socket import timeout as SocketTimeout
+
+import dbase
+from daemon import Daemon
+
+#-------------------------------------------
+# Change the following to suit your system
+#
+
+# MPD Info
+HOST = 'localhost'
+PORT = '6600'
+#If no password, set to False
+PASSWORD = False
+
+# DB Info
+DB_PATH = "/home/marc/projects/mpdsd/stats.db"
+
+# How often to poll MPD (in seconds)
+# The lower the poll frequency, the more accurate listening time
+# will be, but will use more resources.
+POLL_FREQUENCY = 1
+
+# How far into the song to add it
+# as a fraction of the songlength
+# Make sure < 1, and remember that poll frequency may cause innaccuracies
+# as well, if threshold is high.
+# to add at beginning of a song, set to 0
+ADD_THRESHOLD = 0.2
+
+# The default stats template
+STATS_TEMPLATE = "/home/marc/projects/mpdsd/template.py"
+
+#
+# Configuration ends here
+#-------------------------------------------
+
+CONN_ID = {'host':HOST, 'port':PORT}
+
+
+def mpdConnect(client, conn_id):
+        """
+        Connect to mpd
+        """
+        try:
+                client.connect(**conn_id)
+        except (mpd.MPDError, SocketError):
+                print "Could not connect to %s: %s" %(HOST, PORT)
+                return False
+        except:
+                print "Unexpected error: %s", sys.exec_info()[0]
+                return False
+        else:
+                print "Connected to %s:%s" %(HOST, PORT)
+                return True
+
+
+def mpdAuth(client, pword):
+        """
+        Authenticate mpd connection
+        """
+        try:
+                client.password(pword)
+        except mpd.CommandError:
+                print "Could not authenticate"
+                return False
+        except mpd.ConnectionError:
+                print "Problems connecting to %s:%s" %(HOST, PORT)
+                return False
+        except:
+                print "Unexpected error: %s", sys.exc_value
+                return False
+        else:
+                print "Authenticated to %s:%s" %(HOST, PORT)
+                return True
+
+
+def mpdGetStatus(client):
+        """
+        Get the status of mpd
+        """
+        try:
+                return client.status()
+        except mpd.CommandError:
+                print "Could not get status"
+                return False
+        except mpd.ConnectionError:
+                print "Problems connecting to %s:%s" %(HOST, PORT)
+                return False
+        except:
+                print "Unexpected error:", sys.exc_value
+                return False
+        else:
+                return True
+
+
+def mpdCurrentSong(client):
+        """
+        Get the current song
+        """
+        try:
+                return client.currentsong()
+        except (mpd.MPDError, SocketTimeout), err:
+                print "Could not get status:"
+                print "\t", err
+                return False
+
+
+def eventLoop(client, db):
+        trackID = -1    # let -1 mean that no track has been played yet.
+        total = 0       # total time played in the track
+        prevDate = None # the time when the previous was added
+        while True:
+                status = mpdGetStatus(client)
+                if not status:
+                        client.disconnect();
+                        while not mpdConnect(client, CONN_ID):
+                                print "Attempting reconnect"
+                                time.sleep(POLL_FREQUENCY)
+                        print "Connected!"
+                        if PASSWORD:
+                                mpdAuth(client, PASSWORD)
+                elif status['state'] == 'play':
+                        currentSong = mpdCurrentSong(client)
+                        total = total + POLL_FREQUENCY
+                        #print "Current total: " + str(total)
+                        #print "Current time: " + str(status['time'].rsplit(':')[0])
+                        if currentSong['id'] != trackID:
+                                if prevDate != None:
+                                        db.execute('''UPDATE listened SET listentime=? WHERE date=?''', (total,prevDate))
+                                        db.commit()
+                                total = int(status['time'].rsplit(':')[0])
+                                if total >= ADD_THRESHOLD*int(currentSong['time']):
+                                        print currentSong['title']
+                                        prevDate = dbase.dbUpdate(db, currentSong)
+                                        trackID = currentSong['id']
+                elif status['state'] == 'stop':
+                        if prevDate != None:
+                                db.execute('''UPDATE listened SET listentime=? WHERE date=?''', (total, prevDate))
+                                db.commit()
+                                total = 0
+                                prevDate = None
+                time.sleep(POLL_FREQUENCY)
+
+
+def checkConfig():
+        if POLL_FREQUENCY >= 1:
+                print "Error: Poll Frequency must be < 1"
+                
+
+class mpdStatsDaemon(Daemon):
+        def run(self):
+                client = mpd.MPDClient()
+                db = dbase.dbConnect(DB_PATH)
+               
+                while True:
+                        while not mpdConnect(client, CONN_ID):
+                                print "Attempting reconnect"
+                                time.sleep(POLL_FREQUENCY)
+                        print "Connected!"
+                        if PASSWORD:
+                                mpdAuth(client, PASSWORD)
+                        
+                        eventLoop(client, db)
+
+                mpdGetStatus(client)
+                print mpdGetStatus(client)
+
+                client.disconnect()
+
+#
+if __name__ == "__main__":
+        daemon = mpdStatsDaemon('/tmp/mpdsd.pid')
+        if len(sys.argv) >= 2:
+                if 'start' == sys.argv[1]:
+                        daemon.start()
+                        #daemon.run()
+                elif 'stop' == sys.argv[1]:
+                        daemon.stop()
+                elif 'restart' == sys.argv[1]:
+                        daemon.restart()
+                elif 'stats' == sys.argv[1]:
+                        if sys.argv[2]:
+                                stats.generateStats(sys.argv[2])
+                        else:
+                                stats.generateStats(STATS_TEMPLATE)
+                else:
+                        print "Unknown command"
+                        print "usage: %s start|stop|restart|stats" % sys.argv[0]
+                        sys.exit(2)
+                sys.exit(0)
+        else:
+                print "usage: %s start|stop|restart|stats" % sys.argv[0]
+                sys.exit(2)
